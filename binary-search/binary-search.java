797d7b29753f0bd3f99f24e50b2c3f6185f6fbce
Yiming Li <liyiminginireland@gmail.com>
2025-01-03 16:59:51 +0000

    Synchronize existing submission history (1/1)

---
+++ binary-search/binary-search.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+        while(left < right) {
+            int middle = (left + right) / 2;
+
+            if(nums[middle] < target) {
+                left = middle + 1;
+            } else if (nums[middle] > target) {
+                right = middle;
+            } else {
+                return middle;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ binary-search/search-insert-position.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int middle;
+        while(left <= right){
+            middle = left + (right - left) / 2;
+            if(nums[middle] > target){
+                right = middle - 1;
+            }else if(nums[middle] < target){
+                left = middle + 1;
+            }else{
+                return middle;
+            }
+        }
+        return right + 1;
+    }
+}
