0a270873c5923fb87ae4b7fc0650057326067654
Yiming Li <liyiminginireland@gmail.com>
2025-01-04 21:02:36 +0000

    LC-minimum-size-subarray-sum [Runtime: N/A; Memory: N/A]

---
+++ sliding-window/minimum-size-subarray-sum.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int[] sortArr = new int[nums.length];
+
+        int left = 0;
+        int right = nums.length - 1;
+        int index = nums.length - 1;
+
+        while(left <= right) {
+            if(nums[left] < nums[right]) {
+                sortArr[index] = nums[right];
+                right--;
+            } else {
+                sortArr[index] = nums[left];
+                left++;
+            }
+            index--;
+        }
+
+        int sub = 0;
+        int count = 1;
+        for(int i = sortArr.length - 1; i >= 0; i--) {
+            
+            sub += sortArr[i];
+            if (sub >= target) {
+                return count;
+            } 
+            count++;
+        }
+
+        return 0;
+    }
+}
