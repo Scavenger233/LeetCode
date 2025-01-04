4eedd099554c808ef17682bd9e9f627919855681
Yiming Li <liyiminginireland@gmail.com>
2025-01-04 16:53:06 +0000

    LC-binary-search [Runtime: 0 ms; Memory: 45.7 MB]

--- array/binary-search.java
+++ array/binary-search.java
@@ -3,18 +3,14 @@ class Solution {
         int left = 0;
         int right = nums.length;
 
-        // [ , ) 
         while(left < right) {
-            // To prevent boundary issue
             int mid = left + (right - left) / 2;
 
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] > target) {
-                //if target is on the left side, we use mid as the right boundry
                 right = mid;
             } else {
-                // If taget is on the right side, middle is already counted in the closure, so we use mid + 1 as the left boundry
                 left = mid + 1;
             }
         }
