eeb1772db8d05d3ed9f3649fee80830a5fe536eb
Yiming Li <liyiminginireland@gmail.com>
2025-01-06 18:09:02 +0000

    LC-minimum-size-subarray-sum [Runtime: N/A; Memory: N/A]

--- prefix-sum/minimum-size-subarray-sum.java
+++ prefix-sum/minimum-size-subarray-sum.java
@@ -1,23 +1,25 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
         // double for loop
-        int result = Integer.MAX_VALUE;
+        int result = Integer.MAX_VALUE; //最后返回值，也就是subarray的长度 or 0
         int subLength = 0;
         int sum = 0;
 
         for (int i = 0; i < nums.length; i++ ){
             sum = 0;
             for (int j = i; j < nums.length; j++) {
-                sum += nums[i];
+                sum += nums[j];
 
                 if (sum >= target) {
                     subLength = j - i + 1;
-                    result = result > subLength ? subLength : result;
+                    //如果result小于subLength就更新result
+                    result = result < subLength ? result : subLength;
+                    // 只要满足了subarray的和大于target就跳出loop
                     break;
                 }
             }
         }
-
+        // 如果result值没变说明没有符合情况的subarray，返回0；反之则返回新的result值，也就是subarray的长度
         return result == Integer.MAX_VALUE ? 0 : result;
     }
 }
