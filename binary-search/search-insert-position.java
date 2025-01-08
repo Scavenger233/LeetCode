22dd5047fbfe13da0a2cbe5372fc585693bd3184
Yiming Li <liyiminginireland@gmail.com>
2025-01-08 15:30:44 +0000

    LC-search-insert-position [Runtime: 0 ms; Memory: 43.2 MB]

--- binary-search/search-insert-position.java
+++ binary-search/search-insert-position.java
@@ -2,17 +2,20 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
-        int middle;
-        while(left <= right){
-            middle = left + (right - left) / 2;
-            if(nums[middle] > target){
-                right = middle - 1;
-            }else if(nums[middle] < target){
-                left = middle + 1;
-            }else{
-                return middle;
+
+
+        while (left <= right) {
+            int mid = left +(right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
-        return right + 1;
+
+        return left;
+
     }
 }
