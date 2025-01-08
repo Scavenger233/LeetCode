a39f606c0e272e11a1635dcbf6dc4018c35f1621
Yiming Li <liyiminginireland@gmail.com>
2025-01-08 13:26:27 +0000

    LC-squares-of-a-sorted-array [Runtime: 1628 ms; Memory: 46.1 MB]

--- sorting/squares-of-a-sorted-array.java
+++ sorting/squares-of-a-sorted-array.java
@@ -1,30 +1,40 @@
+public class l{
+    public static void main(String[] args) {
+        int[] nums = {-4,-1,0,3,10};
+        Solution s = new Solution();
+        int[] ans = s.sortedSquares(nums);
+        for (int i = 0; i < ans.length; i++) {
+            System.out.print(ans[i] + " ");
+        }
+
+    }
+}
+
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int[] sqArr = new int[nums.length];
+        int[] qsArr = new int[nums.length];
 
-        for (int i = 0; i < nums.length; i++) {
-            sqArr[i] = nums[i] * nums[i];
+        for(int i = 0; i < nums.length; i++) {
+            qsArr[i] = nums[i] * nums[i];
         }
+        int[] sortedArr = new int[nums.length];
+        for (int i = 0; i < qsArr.length; i++) {
+            int left = 0;
+            int right = qsArr.length - 1;
+            int index = qsArr.length - 1;
 
-        int[] sortedSquares = new int[nums.length];
-
-        int left = 0;
-        int right = sortedSquares.length - 1;
-        int index = sortedSquares.length - 1;
-
-        while (left <= right) {
-            if(sqArr[left] > sqArr[right]) {
-                sortedSquares[index] = sqArr[left];
-                left++;
-            } else {
-                sortedSquares[index] = sqArr[right];
-                right--;
+            while (left <= right) {
+                if (qsArr[left] > qsArr[right]) {
+                    sortedArr[index] = qsArr[left];
+                    left++;
+                } else {
+                    sortedArr[index] = qsArr[right];
+                    right--;
+                }
+                index--;
             }
-            index--;
         }
 
-        return sortedSquares;
-
-
+        return sortedArr;
     }
 }
