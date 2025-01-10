57bd098dd690a1dd4b56db427a90421d77b81beb
Yiming Li <liyiminginireland@gmail.com>
2025-01-10 18:13:50 +0000

    LC-find-first-and-last-position-of-element-in-sorted-array [Runtime: 0 ms; Memory: 46 MB]

--- binary-search/find-first-and-last-position-of-element-in-sorted-array.java
+++ binary-search/find-first-and-last-position-of-element-in-sorted-array.java
@@ -4,51 +4,52 @@ class Solution {
         int rightBorder = rightBorder(nums, target);
 
         if (leftBorder == -2 || rightBorder == -2) {
-            return new int[]{-1, -1};
+            return new int[] {-1, -1};
         }
 
-        if (rightBorder - leftBorder >1) {
-            return new int[]{leftBorder + 1, rightBorder - 1};
+        if (rightBorder - leftBorder > 1) {
+            return new int[] {leftBorder + 1, rightBorder -1};
         }
+
+        return new int[] {-1, -1};
+
         
-        return new int[]{-1, -1};
     }
 
-    int leftBorder(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        int leftBorder = -2;
+    int leftBorder(int[] nums, int target) { 
+            int left = 0;
+            int right = nums.length - 1;
+            int leftBorder = -2;
 
-        // To find left border
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] >= target ) {
-                right = mid - 1;
-                leftBorder = right;
-            } else {
-                left = mid + 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] >= target) { 
+                    right = mid - 1;
+                    leftBorder = right;
+                } else {
+                    left = mid + 1;
+                }
             }
+
+            return leftBorder;
         }
 
-        return leftBorder;
-    }
+        int rightBorder(int[] nums, int target) {
+            int left = 0; 
+            int right = nums.length - 1;
+            int rightBorder = -2;
 
-    int rightBorder(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        int rightBorder = -2;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
 
-        // To find right border
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] <= target) {
-                left = mid + 1;
-                rightBorder = left;
-            } else{
-                right = mid - 1;
+                if (nums[mid] <= target) {
+                    left = mid + 1;
+                    rightBorder = left;
+                } else {
+                    right = mid - 1;
+                }
             }
-        }
 
-        return rightBorder;
-    }
+            return rightBorder;
+        }
 }
