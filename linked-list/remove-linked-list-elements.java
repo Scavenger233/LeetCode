f1ba794356a28a3746e69e20b4e419cac442f760
Yiming Li <liyiminginireland@gmail.com>
2025-01-21 18:03:18 +0000

    LC-remove-linked-list-elements [Runtime: 1 ms; Memory: 45.5 MB]

--- linked-list/remove-linked-list-elements.java
+++ linked-list/remove-linked-list-elements.java
@@ -10,22 +10,18 @@
  */
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
-        // Second time
         ListNode dummy = new ListNode();
-        // Set head as dummy and point to the original head
         dummy.next = head;
-        // Set dummy as the new head
         ListNode cur = dummy;
 
         while (cur.next != null) {
-            if (cur.next.val == val) {
-                cur.next = cur.next.next;
-            } else {
+            if (cur.next.val != val) {
                 cur = cur.next;
+            } else {
+                cur.next = cur.next.next;
             }
         }
 
-        // This is the real new head after rearrange the array
         return dummy.next;
     }
 }
