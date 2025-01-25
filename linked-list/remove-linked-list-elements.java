5704caab2a738fa0371bb305e237fcdc99b207e9
Yiming Li <liyiminginireland@gmail.com>
2025-01-25 22:56:49 +0000

    LC-remove-linked-list-elements [Runtime: 1 ms; Memory: 45.5 MB]

--- linked-list/remove-linked-list-elements.java
+++ linked-list/remove-linked-list-elements.java
@@ -12,11 +12,15 @@ class Solution {
     public ListNode removeElements(ListNode head, int val) {
         ListNode dummy = new ListNode();
         dummy.next = head;
+        // cursor is on dummynode
         ListNode cur = dummy;
 
+        //check when the node next to the corsor is null
         while (cur.next != null) {
+            // move one node forward if it's not equal
             if (cur.next.val != val) {
                 cur = cur.next;
+                //move the after one step forward if the node needs to be deleted
             } else {
                 cur.next = cur.next.next;
             }
