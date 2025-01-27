a92c121f2b61288cd9c49c8fad0da2ebaa5cec84
Yiming Li <liyiminginireland@gmail.com>
2025-01-27 12:17:37 +0000

    LC-linked-list-cycle [Runtime: 58 ms; Memory: 19.9 MB]

--- linked-list/linked-list-cycle.py
+++ linked-list/linked-list-cycle.py
@@ -6,14 +6,16 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        cur = head
+        # 这道题不需要cur指针，因为cur指针用在需要调整链表结构的情况下
+        # 本题只需要移动指针
+        slow, fast = head, head
         
-        while cur and cur.next and cur.next.next:
-            slow, fast = cur, cur
-            if slow != fast:
-                slow = slow.next
-                fast = fast.next.next
-            else:
+        while fast and fast.next:
+            # 进循环需要先移动指针，而不是比较，因为这时slow和fast是相等的
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
                 return True
         
         return False
