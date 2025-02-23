7fa7602c71352d74e5c217482daaf9e64c80938b
Yiming Li <liyiminginireland@gmail.com>
2025-02-23 14:52:39 +0000

    LC-min-stack [Runtime: 3 ms; Memory: 21.3 MB]

--- stack/min-stack.py
+++ stack/min-stack.py
@@ -16,8 +16,14 @@ class MinStack:
 
     def top(self) -> int:
         return self.stack[-1]
-        
 
     def getMin(self) -> int:
         return self.minStack[-1]
-        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
