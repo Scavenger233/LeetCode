b61c45878a94e00c5472744bd75db2a4b0bae24b
Yiming Li <liyiminginireland@gmail.com>
2025-02-23 13:32:43 +0000

    LC-valid-parentheses [Runtime: 2 ms; Memory: 17.7 MB]

--- string/valid-parentheses.py
+++ string/valid-parentheses.py
@@ -11,6 +11,5 @@ class Solution:
                     return False
             else:
                 stack.append(c)
-        return True if not stack else False
-
         
+        return not stack
