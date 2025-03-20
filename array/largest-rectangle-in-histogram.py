92d69895acea7f5c151adca9a02e49c47962a53b
Yiming Li <liyiminginireland@gmail.com>
2025-03-20 09:29:45 +0000

    LC-largest-rectangle-in-histogram [Runtime: 165 ms; Memory: 36 MB]

--- array/largest-rectangle-in-histogram.py
+++ array/largest-rectangle-in-histogram.py
@@ -1,7 +1,7 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         maxArea = 0
-        stack = [] # (index, height)
+        stack = []
 
         for i, h in enumerate(heights):
             start = i
@@ -10,7 +10,7 @@ class Solution:
                 maxArea = max(maxArea, height * (i - index))
                 start = index
             stack.append((start, h))
-        
+
         for i, h in stack:
             maxArea = max(maxArea, h * (len(heights) - i))
         
