f2bcff292077d0853b3af2896fbc78494c174c8b
Yiming Li <liyiminginireland@gmail.com>
2025-03-26 15:04:59 +0000

    LC-find-minimum-in-rotated-sorted-array [Runtime: 0 ms; Memory: 18 MB]

--- binary-search/find-minimum-in-rotated-sorted-array.py
+++ binary-search/find-minimum-in-rotated-sorted-array.py
@@ -1,4 +1,20 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        nums.sort(reverse=False)
-        return nums[0]
+        res = nums[0]
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            if nums[l] <= nums[r]:
+                res = min(res, nums[l])
+                break
+            
+            m = (l + r) // 2
+            if nums[l] <= nums[m]:
+                l = m + 1
+            else:
+                r = m - 1
+            
+            res = min(res, nums[m])
+
+        return res 
+            
