85a4f8665af5238008f772e4b884be350487edd6
Yiming Li <liyiminginireland@gmail.com>
2025-03-28 18:17:56 +0000

    LC-koko-eating-bananas [Runtime: 214 ms; Memory: 18.8 MB]

--- binary-search/koko-eating-bananas.py
+++ binary-search/koko-eating-bananas.py
@@ -2,7 +2,6 @@ class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         l, r = 1, max(piles)
         res = r
-
         while l <= r:
             k = (l + r) // 2
             totalTime = 0
@@ -15,4 +14,6 @@ class Solution:
                 r = k - 1
             else:
                 l = k + 1
+        
         return res
+                
