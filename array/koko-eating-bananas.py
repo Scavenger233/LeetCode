482b1390d14eabb8da1ab915bcc8d913957de127
Yiming Li <liyiminginireland@gmail.com>
2025-03-29 22:07:16 +0000

    LC-koko-eating-bananas [Runtime: 220 ms; Memory: 19 MB]

--- array/koko-eating-bananas.py
+++ array/koko-eating-bananas.py
@@ -1,7 +1,9 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
         l, r = 1, max(piles)
         res = r
+
         while l <= r:
             k = (l + r) // 2
             totalTime = 0
@@ -9,11 +11,11 @@ class Solution:
             for p in piles:
                 totalTime += math.ceil(float(p) / k)
 
+            k = (l + r) // 2
+
             if totalTime <= h:
                 res = k
                 r = k - 1
             else:
                 l = k + 1
-        
         return res
-                
