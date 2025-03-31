d16861efc4cd426f7a0c7c6892cbe8070f62b4bf
Yiming Li <liyiminginireland@gmail.com>
2025-03-31 22:03:02 +0100

    LC-find-minimum-in-rotated-sorted-array [Runtime: 0 ms; Memory: 18.1 MB]

--- array/find-minimum-in-rotated-sorted-array.py
+++ array/find-minimum-in-rotated-sorted-array.py
@@ -4,13 +4,13 @@ class Solution:
         res = nums[0]
 
         while l <= r:
-            if nums[l] < nums[r]:
+            if nums[l] < nums[r]: # There can't be equal, or else it will affect the next judgement
                 res = min(res, nums[l])
                 break
             
             m = (l + r) // 2
+            # the value of res must be assigned before if condition
             res = min(res, nums[m])
-            
             if nums[l] <= nums[m]:
                 l = m + 1
             else:
