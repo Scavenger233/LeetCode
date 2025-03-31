19cc58f91a694fb8d5a13d5535278628d7b31e33
Yiming Li <liyiminginireland@gmail.com>
2025-03-31 21:40:08 +0100

    LC-koko-eating-bananas [Runtime: 222 ms; Memory: 19 MB]

--- binary-search/koko-eating-bananas.py
+++ binary-search/koko-eating-bananas.py
@@ -1,21 +1,21 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        
         l, r = 1, max(piles)
         res = r
 
         while l <= r:
             k = (l + r) // 2
             totalTime = 0
-
             for p in piles:
                 totalTime += math.ceil(float(p) / k)
 
-            k = (l + r) // 2
 
             if totalTime <= h:
-                res = k
+                res = k # res only updates when total time is smaller than h
                 r = k - 1
             else:
                 l = k + 1
+
+            
+
         return res
