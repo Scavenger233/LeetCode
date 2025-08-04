c66b8fcd023d70f3fb5951cb86ae30d3606ebe0d
Yiming Li <liyiminginireland@gmail.com>
2025-08-04 16:01:31 +0100

    LC-valid-anagram [Runtime: 14 ms; Memory: 17.9 MB]

--- hash-table/valid-anagram.py
+++ hash-table/valid-anagram.py
@@ -3,8 +3,8 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        countS, countT = {}, {}
-        
+        countS, countT = {},{}
+
         for i in range(len(s)):
             countS[s[i]] = 1 + countS.get(s[i], 0)
             countT[t[i]] = 1 + countT.get(t[i], 0)
