c1490eeabaa31ab7c6411e9f6710881766066fab
Yiming Li <liyiminginireland@gmail.com>
2025-08-10 20:00:10 +0100

    LC-top-k-frequent-elements [Runtime: 11 ms; Memory: 24.1 MB]

--- bucket-sort/top-k-frequent-elements.py
+++ bucket-sort/top-k-frequent-elements.py
@@ -7,10 +7,10 @@ class Solution:
             count[num] = 1 + count.get(num, 0)
         for n, c in count.items():
             freq[c].append(n)
-        
+
         res = []
         for i in range(len(freq) - 1, 0, -1):
             for n in freq[i]:
                 res.append(n)
-                if len(res) == k:
-                    return res
+            if len(res) == k:
+                return res
